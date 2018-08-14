d4c7d05bd245a5c28647973ed90c13356a312ef3
ljwkevin <18328091428@163.com>
2018-08-14 18:45:18 +0800

    TEST

--- src/main/java/com/citi/ordermatching/dao/DealRecordMapper.java
+++ src/main/java/com/citi/ordermatching/dao/DealRecordMapper.java
@@ -3,6 +3,8 @@ package com.citi.ordermatching.dao;
 import com.citi.ordermatching.domain.DealRecord;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface DealRecordMapper {
     int deleteByPrimaryKey(Integer id);
@@ -16,4 +18,6 @@ public interface DealRecordMapper {
     int updateByPrimaryKeySelective(DealRecord record);
 
     int updateByPrimaryKey(DealRecord record);
+
+    List<DealRecord> findAllDealRecord();
 }
