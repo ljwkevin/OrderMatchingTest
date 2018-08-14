517552c01c35e2cacb7a4b48c2968ee5ff5de0ff
ljwkevin <18328091428@163.com>
2018-08-14 13:45:05 +0800

    TEST

---
+++ src/main/java/com/citi/ordermatching/domain/OrderStock.java
@@ -0,0 +1,44 @@
+package com.citi.ordermatching.domain;
+
+/**
+ * Created by Dell on 2018/8/14.
+ */
+public class OrderStock {
+
+    private double bid;
+    private double ask;
+    private int bidSize;
+    private int askSize;
+
+    public double getBid() {
+        return bid;
+    }
+
+    public void setBid(double bid) {
+        this.bid = bid;
+    }
+
+    public double getAsk() {
+        return ask;
+    }
+
+    public void setAsk(double ask) {
+        this.ask = ask;
+    }
+
+    public int getBidSize() {
+        return bidSize;
+    }
+
+    public void setBidSize(int bidSize) {
+        this.bidSize = bidSize;
+    }
+
+    public int getAskSize() {
+        return askSize;
+    }
+
+    public void setAskSize(int askSize) {
+        this.askSize = askSize;
+    }
+}
