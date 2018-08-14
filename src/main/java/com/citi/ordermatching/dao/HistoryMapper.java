c46f408e15511e5310fc44f27938ea77d99d2f73
ljwkevin <18328091428@163.com>
2018-08-14 14:51:35 +0800

    TEST

---
+++ src/main/java/com/citi/ordermatching/dao/HistoryMapper.java
@@ -0,0 +1,24 @@
+package com.citi.ordermatching.dao;
+
+import com.citi.ordermatching.domain.History;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+@Mapper
+public interface HistoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(History record);
+
+    int insertSelective(History record);
+
+    History selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(History record);
+
+    int updateByPrimaryKey(History record);
+
+    List<History> findAllHistory();
+}
