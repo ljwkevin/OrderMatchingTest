d9a9c97459391545584e0cef754398252e90f2f2
JedreckZHou <jedreckzhou@gmail.com>
2018-08-14 17:36:07 +0800

    a

--- src/main/java/com/citi/ordermatching/serviceImpl/HistoryServiceImpl.java
+++ src/main/java/com/citi/ordermatching/serviceImpl/HistoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.citi.ordermatching.serviceImpl;
 
 import com.citi.ordermatching.dao.HistoryMapper;
 import com.citi.ordermatching.domain.History;
+import com.citi.ordermatching.enums.OrderStatus;
 import com.citi.ordermatching.service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,21 @@ public class HistoryServiceImpl implements HistoryService {
     @Override
     public List<History> findAllHistory() {
         return historyMapper.findAllHistory();
+    }
+
+    @Override
+    public void insertHistory(History history) {
+        historyMapper.insertSelective(history);
+    }
+
+    @Override
+    public History findById(int id) {
+        return historyMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public boolean cancelOrder(History history) {
+        history.setStatus(OrderStatus.CANCELLED.toString());
+        return historyMapper.updateByPrimaryKeySelective(history)>0 ? true : false;
     }
 }
