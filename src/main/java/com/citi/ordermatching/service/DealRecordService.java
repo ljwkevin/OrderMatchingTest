f71d8c28f5abd56d18d9ed1dbc61fe9d30369135
ljwkevin <18328091428@163.com>
2018-08-14 18:46:12 +0800

    TEST

--- src/main/java/com/citi/ordermatching/service/DealRecordService.java
+++ src/main/java/com/citi/ordermatching/service/DealRecordService.java
@@ -3,7 +3,9 @@ package com.citi.ordermatching.service;
 import com.citi.ordermatching.domain.DealRecord;
 
 import java.util.Date;
+import java.util.List;
 
 public interface DealRecordService {
     public void addRecord(Date dealTime, double dealPrice, int dealSize, int bidOrderId, int askOrderId);
+    public List<DealRecord> findAllRecord();
 }
