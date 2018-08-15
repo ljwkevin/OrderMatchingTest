4d2f8c880ec8943aae5290ab3a41adced65d1f71
ljwkevin <18328091428@163.com>
2018-08-15 17:50:49 +0800

    TEST

--- src/main/java/com/citi/ordermatching/controller/HistoryController.java
+++ src/main/java/com/citi/ordermatching/controller/HistoryController.java
@@ -2,7 +2,9 @@ package com.citi.ordermatching.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.citi.ordermatching.domain.History;
+import com.citi.ordermatching.service.DealRecordService;
 import com.citi.ordermatching.service.HistoryService;
+import com.citi.ordermatching.service.OrderbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +24,14 @@ public class HistoryController {
     @Autowired
     private HistoryService historyService;
 
+
+
+
+
     @RequestMapping("findAllHistory")
     public String findAllHistory(){
-        List<History> historyList=historyService.findAllHistory();
-        String resultJSON= JSON.toJSONString(historyList);
+
+        String resultJSON= historyService.findAllHistory();
         return resultJSON;
     }
 
@@ -34,4 +40,6 @@ public class HistoryController {
         History history = historyService.findById(historyId);
         return historyService.cancelOrder(history);
     }
+
+
 }
